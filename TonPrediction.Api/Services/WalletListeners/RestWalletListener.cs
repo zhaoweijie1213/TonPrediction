5512158a7c08b43a438066f5f96c4dd86567bde4
zhaoweijie <zhaoweijie1108@qq.com>
2025-07-11 18:09:37 +0800

    修改rest api监听

--- TonPrediction.Api/Services/WalletListeners/RestWalletListener.cs
+++ TonPrediction.Api/Services/WalletListeners/RestWalletListener.cs
@@ -19,7 +19,7 @@ public class RestWalletListener(ILogger<RestWalletListener> logger, IHttpClientF
     {
         while (!ct.IsCancellationRequested)
         {
-            var url = $"/v2/blockchain/accounts/{walletAddress}/transactions?limit=5&to_lt={lastLt}";
+            var url = $"/v2/blockchain/accounts/{walletAddress}/transactions?limit=20&to_lt={lastLt}";
             var resp = await _http.GetAsync(url, ct);
 
             var content = await resp.Content.ReadAsStringAsync(ct);
